027c46ee9dfb3ad52cd7f372c417abe902363b6d
QH\wa-najaria <amirhossein.najari@health.qld.gov.au>
2023-06-21 13:33:43 +1000

    Dark tables need testing and fixations

--- src/components/data_table/js/global.js
+++ src/components/data_table/js/global.js
@@ -77,9 +77,11 @@
                     language: {
                         paginate: {
                             next: `<a rel="next" aria-label="Next page of results" class="qld__search-pagination_link">
-                            <span>Next</span>
+                            <span>Next</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" aria-hidden="true" focusable="false" width="16" height="16" role="img"><path fill="currentColor" d="M443.7 266.8l-165.9 176C274.5 446.3 269.1 448 265.5 448c-3.986 0-7.988-1.375-11.16-4.156c-6.773-5.938-7.275-16.06-1.118-22.59L393.9 272H16.59c-9.171 0-16.59-7.155-16.59-15.1S7.421 240 16.59 240h377.3l-140.7-149.3c-6.157-6.531-5.655-16.66 1.118-22.59c6.789-5.906 17.27-5.469 23.45 1.094l165.9 176C449.4 251.3 449.4 260.7 443.7 266.8z"></path></svg>
                             </a>`,
-                            previous: ''
+                            previous: `<a rel="prev" aria-label="Next page of results" class="qld__search-pagination_link">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" aria-hidden="true" focusable="false" width="16" height="16" role="img"><path fill="currentColor" d="M448 256C448 264.8 440.6 272 431.4 272H54.11l140.7 149.3c6.157 6.531 5.655 16.66-1.118 22.59C190.5 446.6 186.5 448 182.5 448c-4.505 0-9.009-1.75-12.28-5.25l-165.9-176c-5.752-6.094-5.752-15.41 0-21.5l165.9-176c6.19-6.562 16.69-7 23.45-1.094c6.773 5.938 7.275 16.06 1.118 22.59L54.11 240h377.3C440.6 240 448 247.2 448 256z"></path></svg><span>Back</span>
+                            </a>`
                         }
                     },
                     columnDefs: [
