b2acdfe6cfd6bf5d4bdf0699a8003eb0ae2cadd7
bsellars <bsellars@squiz.co.nz>
2023-07-19 15:11:58 +1200

    Rebuilt

--- dist/components/breadcrumbs/presentation.js
+++ dist/components/breadcrumbs/presentation.js
@@ -6,11 +6,11 @@
         return undefined
     };
 
-  return "<nav class=\"qld__breadcrumbs\" aria-label=\"breadcrumb\">\r\n    <ul class=\"qld__breadcrumbs__list--desktop qld__link-list qld__link-list--inline\">\r\n"
+  return "<nav class=\"qld__breadcrumbs\" aria-label=\"breadcrumb\">\n    <ul class=\"qld__breadcrumbs__list--desktop qld__link-list qld__link-list--inline\">\n"
     + ((stack1 = lookupProperty(helpers,"each").call(alias1,((stack1 = (depth0 != null ? lookupProperty(depth0,"current") : depth0)) != null ? lookupProperty(stack1,"lineage") : stack1),{"name":"each","hash":{},"fn":container.program(2, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":4,"column":8},"end":{"line":12,"column":17}}})) != null ? stack1 : "")
-    + "    </ul>\r\n    <ul class=\"qld__breadcrumbs__list--mobile qld__link-list qld__link-list--inline\">\r\n"
+    + "    </ul>\n    <ul class=\"qld__breadcrumbs__list--mobile qld__link-list qld__link-list--inline\">\n"
     + ((stack1 = lookupProperty(helpers,"with").call(alias1,(lookupProperty(helpers,"itemAt")||(depth0 && lookupProperty(depth0,"itemAt"))||container.hooks.helperMissing).call(alias1,((stack1 = (depth0 != null ? lookupProperty(depth0,"current") : depth0)) != null ? lookupProperty(stack1,"lineage") : stack1),-2,{"name":"itemAt","hash":{},"data":data,"loc":{"start":{"line":15,"column":16},"end":{"line":15,"column":43}}}),{"name":"with","hash":{},"fn":container.program(12, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":15,"column":8},"end":{"line":25,"column":17}}})) != null ? stack1 : "")
-    + "    </ul>\r\n</nav>\r\n\r\n";
+    + "    </ul>\n</nav>\n\n";
 },"2":function(container,depth0,helpers,partials,data) {
     var stack1, alias1=depth0 != null ? depth0 : (container.nullContext || {}), lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
@@ -21,9 +21,9 @@
 
   return "            <li "
     + ((stack1 = lookupProperty(helpers,"if").call(alias1,(data && lookupProperty(data,"last")),{"name":"if","hash":{},"fn":container.program(3, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":5,"column":16},"end":{"line":5,"column":55}}})) != null ? stack1 : "")
-    + ">\r\n"
+    + ">\n"
     + ((stack1 = lookupProperty(helpers,"if").call(alias1,(data && lookupProperty(data,"last")),{"name":"if","hash":{},"fn":container.program(5, data, 0),"inverse":container.program(7, data, 0),"data":data,"loc":{"start":{"line":6,"column":16},"end":{"line":10,"column":23}}})) != null ? stack1 : "")
-    + "            </li>\r\n";
+    + "            </li>\n";
 },"3":function(container,depth0,helpers,partials,data) {
     return "aria-current=\"page\"";
 },"5":function(container,depth0,helpers,partials,data) {
@@ -36,7 +36,7 @@
 
   return "                    "
     + container.escapeExpression((lookupProperty(helpers,"renderSpecialChar")||(depth0 && lookupProperty(depth0,"renderSpecialChar"))||container.hooks.helperMissing).call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? lookupProperty(depth0,"asset_short_name") : depth0),{"name":"renderSpecialChar","hash":{},"data":data,"loc":{"start":{"line":7,"column":20},"end":{"line":7,"column":58}}}))
-    + "\r\n";
+    + "\n";
 },"7":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
@@ -49,7 +49,7 @@
     + container.escapeExpression(((helper = (helper = lookupProperty(helpers,"asset_url") || (depth0 != null ? lookupProperty(depth0,"asset_url") : depth0)) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(alias1,{"name":"asset_url","hash":{},"data":data,"loc":{"start":{"line":9,"column":25},"end":{"line":9,"column":38}}}) : helper)))
     + "\">"
     + ((stack1 = lookupProperty(helpers,"if").call(alias1,(data && lookupProperty(data,"first")),{"name":"if","hash":{},"fn":container.program(8, data, 0),"inverse":container.program(10, data, 0),"data":data,"loc":{"start":{"line":9,"column":40},"end":{"line":9,"column":111}}})) != null ? stack1 : "")
-    + "</a>\r\n";
+    + "</a>\n";
 },"8":function(container,depth0,helpers,partials,data) {
     return "Home";
 },"10":function(container,depth0,helpers,partials,data) {
@@ -69,13 +69,13 @@
         return undefined
     };
 
-  return "            <li>\r\n            <a href=\""
+  return "            <li>\n            <a href=\""
     + container.escapeExpression(((helper = (helper = lookupProperty(helpers,"asset_url") || (depth0 != null ? lookupProperty(depth0,"asset_url") : depth0)) != null ? helper : alias2),(typeof helper === "function" ? helper.call(alias1,{"name":"asset_url","hash":{},"data":data,"loc":{"start":{"line":17,"column":21},"end":{"line":17,"column":34}}}) : helper)))
-    + "\">\r\n"
+    + "\">\n"
     + ((stack1 = (lookupProperty(helpers,"ifCond")||(depth0 && lookupProperty(depth0,"ifCond"))||alias2).call(alias1,(depth0 != null ? lookupProperty(depth0,"asset_is_site_asset") : depth0),"==","1",{"name":"ifCond","hash":{},"fn":container.program(13, data, 0),"inverse":container.program(15, data, 0),"data":data,"loc":{"start":{"line":18,"column":16},"end":{"line":22,"column":27}}})) != null ? stack1 : "")
-    + "            </a>    \r\n        </li>\r\n";
+    + "            </a>    \n        </li>\n";
 },"13":function(container,depth0,helpers,partials,data) {
-    return "                    Home\r\n";
+    return "                    Home\n";
 },"15":function(container,depth0,helpers,partials,data) {
     var lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
@@ -86,7 +86,7 @@
 
   return "                   "
     + container.escapeExpression((lookupProperty(helpers,"renderSpecialChar")||(depth0 && lookupProperty(depth0,"renderSpecialChar"))||container.hooks.helperMissing).call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? lookupProperty(depth0,"asset_short_name") : depth0),{"name":"renderSpecialChar","hash":{},"data":data,"loc":{"start":{"line":21,"column":19},"end":{"line":21,"column":57}}}))
-    + "\r\n";
+    + "\n";
 },"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
