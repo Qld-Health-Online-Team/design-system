99d9a6be0029e0e4a6e196fd5931deb57a579860
QH\wa-najaria <amirhossein.najari@health.qld.gov.au>
2023-11-03 11:57:45 +1000

    building the blockquote fix - dept-171

--- dist/components/data_table/presentation.js
+++ dist/components/data_table/presentation.js
@@ -8,21 +8,21 @@
 
   return "<section class=\"qld__body "
     + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? lookupProperty(depth0,"metadata") : depth0)) != null ? lookupProperty(stack1,"body_background") : stack1)) != null ? lookupProperty(stack1,"value") : stack1), depth0))
-    + "\">\n    <div class=\"container-fluid\">\n        <div class=\"qld__data-table "
+    + "\">\r\n    <div class=\"container-fluid\">\r\n        <div class=\"qld__data-table "
     + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? lookupProperty(depth0,"metadata") : depth0)) != null ? lookupProperty(stack1,"table_data_source") : stack1)) != null ? lookupProperty(stack1,"value") : stack1), depth0))
     + " "
     + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? lookupProperty(depth0,"metadata") : depth0)) != null ? lookupProperty(stack1,"table_styling") : stack1)) != null ? lookupProperty(stack1,"value") : stack1), depth0))
     + "\" id=\""
     + ((stack1 = lookupProperty(helpers,"if").call(depth0 != null ? depth0 : (container.nullContext || {}),((stack1 = ((stack1 = (depth0 != null ? lookupProperty(depth0,"metadata") : depth0)) != null ? lookupProperty(stack1,"id_field") : stack1)) != null ? lookupProperty(stack1,"value") : stack1),{"name":"if","hash":{},"fn":container.program(2, data, 0),"inverse":container.program(4, data, 0),"data":data,"loc":{"start":{"line":5,"column":111},"end":{"line":5,"column":210}}})) != null ? stack1 : "")
-    + "\">\n            <div class=\"qld__data-table__header\">\n                <span class=\"qld__display-md\">"
+    + "\">\r\n            <div class=\"qld__data-table__header\">\r\n                <span class=\"qld__display-md\">"
     + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? lookupProperty(depth0,"metadata") : depth0)) != null ? lookupProperty(stack1,"table_title") : stack1)) != null ? lookupProperty(stack1,"value") : stack1), depth0))
-    + "</span>\n                <span class=\"qld__caption\">\n                    "
+    + "</span>\r\n                <span class=\"qld__caption\">\r\n                    "
     + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? lookupProperty(depth0,"metadata") : depth0)) != null ? lookupProperty(stack1,"table_caption") : stack1)) != null ? lookupProperty(stack1,"value") : stack1), depth0))
-    + "\n                </span>\n            </div>\n            <div id=\"qld_data-table_html\" class=\"qld__data-table-content\" aria-describedby=\"\" style=\"display: none\">\n                "
+    + "\r\n                </span>\r\n            </div>\r\n            <div id=\"qld_data-table_html\" class=\"qld__data-table-content\" aria-describedby=\"\" style=\"display: none\">\r\n                "
     + ((stack1 = alias1(((stack1 = ((stack1 = (depth0 != null ? lookupProperty(depth0,"metadata") : depth0)) != null ? lookupProperty(stack1,"table_in_html") : stack1)) != null ? lookupProperty(stack1,"value") : stack1), depth0)) != null ? stack1 : "")
-    + "\n            </div>\n            <table id=\"qld_data-table_csv\" class=\"qld__data-table-content\" aria-describedby=\"\" data-csv-source=\"./?a="
+    + "\r\n            </div>\r\n            <table id=\"qld_data-table_csv\" class=\"qld__data-table-content\" aria-describedby=\"\" data-csv-source=\"./?a="
     + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? lookupProperty(depth0,"metadata") : depth0)) != null ? lookupProperty(stack1,"table_data_csv") : stack1)) != null ? lookupProperty(stack1,"value") : stack1), depth0))
-    + "\" style=\"display: none\">\n            </table>\n        </div>\n    </div>\n</section>\n";
+    + "\" style=\"display: none\">\r\n            </table>\r\n        </div>\r\n    </div>\r\n</section>\r\n";
 },"2":function(container,depth0,helpers,partials,data) {
     var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
@@ -50,6 +50,6 @@
         return undefined
     };
 
-  return "\n"
+  return "\r\n"
     + ((stack1 = lookupProperty(helpers,"with").call(depth0 != null ? depth0 : (container.nullContext || {}),((stack1 = (depth0 != null ? lookupProperty(depth0,"component") : depth0)) != null ? lookupProperty(stack1,"data") : stack1),{"name":"with","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":2,"column":0},"end":{"line":20,"column":9}}})) != null ? stack1 : "");
 },"useData":true}
