7f6bd7136352c8b43cac7ce74ddc39c8e64730cf
QH\wa-najaria <amirhossein.najari@health.qld.gov.au>
2023-11-14 13:29:35 +1000

    adding border and preparing for review - qhwt-758

--- src/components/global_alert/js/global.js
+++ src/components/global_alert/js/global.js
@@ -20,7 +20,7 @@
         for (const alert of alerts) {
 
             if (siteName?.length > 0) {
-                var alertSeen = QLD.utils.getCookie(siteName + `_alertSeen_${++index}`); //increment index so the next alert can be checked. 
+                var alertSeen = QLD.utils.getCookie(`${siteName}_alertSeen_${++index}`); //increment index so the next alert can be checked. 
                 if (alertSeen) { //if this specific alert is seen and closed, hide it.
                     alert.style.maxHeight = "0";
                     alert.style.display ="none";
@@ -38,7 +38,7 @@
                         alert.style.display ="none";
 
                         if (siteName?.length > 0) {
-                            QLD.utils.setCookie(siteName + `_alertSeen_${++index}`,"true");
+                            QLD.utils.setCookie(`${siteName}_alertSeen_${++index}`,"true");
                         }
                     },
                     false
