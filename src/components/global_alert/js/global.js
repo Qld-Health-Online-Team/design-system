2071523fc476dc7fb90f2a5e320c04cafef358f7
QH\wa-najaria <amirhossein.najari@health.qld.gov.au>
2023-11-14 12:36:11 +1000

    Fixing the alignment and the js to make sure code runs indipendently - qhwt-758

--- src/components/global_alert/js/global.js
+++ src/components/global_alert/js/global.js
@@ -12,17 +12,34 @@
      * @private
      */
     function initGlobalAlert() {
+
         var alerts = document.getElementsByClassName("qld__global-alert") || [];
+        var siteName = document.querySelector(".qld__global-alert__include")?.alertContainer?.getAttribute("data-name");
+        var index = 0;
+
         for (const alert of alerts) {
-            let closeButton = alert.querySelector(
-                ".qld__global-alert__close button"
-            );
+
+            if (siteName.length > 0) {
+                var alertSeen = QLD.utils.getCookie(siteName + `_alertSeen_${++index}`); //increment index so the next alert can be checked. 
+                if (alertSeen) { //if this specific alert is seen and closed, hide it.
+                    alert.style.maxHeight = "0";
+                    alert.style.display ="none";
+                    break; // no need to the rest of the loop, go check the next alert.
+                }
+            }
+
+            let closeButton = alert.querySelector(".qld__global-alert__close button");
+
             if (closeButton !== null) {
                 closeButton.addEventListener(
                     "click",
                     function () {
                         alert.style.maxHeight = "0";
                         alert.style.display ="none";
+
+                        if (siteName.length > 0) {
+                            QLD.utils.setCookie(siteName + `_alertSeen_${++index}`,"true");
+                        }
                     },
                     false
                 );
