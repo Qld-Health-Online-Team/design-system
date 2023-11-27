6c86e06b1e341a45a08f2bc9e7cf3af8aad810c4
TJ Flassman <tflassman@squiz.net>
2023-11-27 17:12:19 +1000

    added a test fo the h1 banner basic

--- cypress/e2e/components/banner_basic.cy.js
+++ cypress/e2e/components/banner_basic.cy.js
@@ -6,12 +6,16 @@ describe("Banner Basic Tests", () => {
         cy.visit(`${url}component-banner_basic.html`);
     });
 
-    it("Banner basic renders correctly", () => {
+    it("Banner basic renders correctly", () => {//unit test
         cy.get(".qld__banner ").should("exist");
         cy.get(".qld__banner__basic").should("exist");
         cy.get(".qld__banner__wrapper").should("exist");
         cy.get(".qld__banner__hero").should("exist");
         cy.get(".qld__banner__content").should("exist");
+        
+    });
+
+    it("Banner title dispalys correctly", () => {//unit test
         cy.get(".qld__banner__content h1").should("exist");
     })
 });
