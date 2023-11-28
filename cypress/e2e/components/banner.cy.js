fda3893a2bbe381f9dd4bfd6b221a20031c63cce
TJ Flassman <tflassman@squiz.net>
2023-11-28 14:50:58 +1000

    add test for the background and text

--- cypress/e2e/components/banner.cy.js
+++ cypress/e2e/components/banner.cy.js
@@ -11,4 +11,30 @@ describe("Banner tests", () => {
         cy.get('.qld__banner__content').should('exist');
         cy.get('.qld__breadcrumbs').should('exist');
     })
+
+    it("Banner renders background color or image", () => { //unit test
+        cy.get('section.qld__banner').then(($background) => {
+            if ($background.hasClass('.qld__banner--alt')) {
+                cy.get('.qld__banner--alt').should('exist');
+            } else if ($background.hasClass('.qld__banner--dark')) {
+                cy.get('.qld__banner--dark').should('exist');
+            } else if ($background.hasClass('.qld__banner--dark-alt')) {
+                cy.get('.qld__banner--dark-alt').should('exist');
+            } else if ($background.hasClass('.qld__banner')){
+                cy.get('section.qld__banner').should('have.attr', 'style');
+            }
+        })
+    })
+
+    it("Banner breadcrumbs render correctly", () => { //unit test
+        cy.get('.qld__link-list.qld__link-list--inline').should('exist');
+        cy.get('.qld__link-list.qld__link-list--inline > li').should('exist');
+        cy.get('li').invoke('text').should('not.be.empty');
+    })
+
+    it("Banner <h1> heading is rendering correctly ", () => { //unit test
+        cy.get('.qld__banner__content > h1').invoke('text').should('not.be.empty');
+    })
+
+   
 });
