a1829440d61d8d3d4dcb8c9a163f3b12fbd3bc8d
TJ Flassman <tflassman@squiz.net>
2023-11-30 15:42:21 +1000

    test for banner advanced

--- cypress/e2e/components/banner_advanced.cy.js
+++ cypress/e2e/components/banner_advanced.cy.js
@@ -6,37 +6,39 @@ describe("Banner tests", () => {
         cy.visit(`${url}component-banner_advanced.html`);
     });
 
-    it("Banner renders correctly", () => { //unit test
-        cy.get('.qld__banner__advanced').should('exist');
+    it("Banner advanced renders correctly", () => { //unit test
+        cy.get('.qld__banner.qld__banner__advanced').should('exist');
         cy.get('.qld__banner__wrapper').should('exist');
         cy.get('.qld__banner__main').should('exist');
         cy.get('.qld__banner__hero').should('exist');
         cy.get('.qld__banner__content').should('exist');
     })
 
-    // it("Banner renders background color or image", () => { //unit test
-    //     cy.get('section.qld__banner').then(($background) => {
-    //         if ($background.hasClass('.qld__banner--alt')) {
-    //             cy.get('.qld__banner--alt').should('exist');
-    //         } else if ($background.hasClass('.qld__banner--dark')) {
-    //             cy.get('.qld__banner--dark').should('exist');
-    //         } else if ($background.hasClass('.qld__banner--dark-alt')) {
-    //             cy.get('.qld__banner--dark-alt').should('exist');
-    //         } else if ($background.hasClass('.qld__banner')){
-    //             cy.get('section.qld__banner').should('have.attr', 'style');
-    //         }
-    //     })
-    // })
+    it("Banner advanced renders background color ", () => { //unit test
+        cy.get('section.qld__banner.qld__banner__advanced').then(($background) => {
+            if ($background.hasClass('.qld__banner--alt')) {
+                cy.get('.qld__banner--alt').should('exist');
+            } else if ($background.hasClass('.qld__banner--dark')) {
+                cy.get('.qld__banner--dark').should('exist');
+            } else if ($background.hasClass('.qld__banner--dark-alt')) {
+                cy.get('.qld__banner--dark-alt').should('exist');
+            }
+        })
+    })
 
-    // it("Banner breadcrumbs render correctly", () => { //unit test
-    //     cy.get('.qld__link-list.qld__link-list--inline').should('exist');
-    //     cy.get('.qld__link-list.qld__link-list--inline > li').should('exist');
-    //     cy.get('li').invoke('text').should('not.be.empty');
-    // })
+    it("Banner advanced renders hero image", () => {
+        cy.get('.qld__banner__hero')
+    }) 
 
-    // it("Banner <h1> heading is rendering correctly ", () => { //unit test
-    //     cy.get('.qld__banner__content > h1').invoke('text').should('not.be.empty');
-    // })
+    it("Banner breadcrumbs render correctly", () => { //unit test
+        cy.get('.qld__link-list.qld__link-list--inline').should('exist');
+        cy.get('.qld__link-list.qld__link-list--inline > li').should('exist');
+        cy.get('li').invoke('text').should('not.be.empty');
+    })
+
+    it("Banner <h1> heading is rendering correctly ", () => { //unit test
+        cy.get('.qld__banner__content > h1').invoke('text').should('not.be.empty');
+    })
 
    
 });
