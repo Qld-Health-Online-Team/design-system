53b879e58a4aad2201e14e41987b61bcd5b44ffa
TJ Flassman <tflassman@squiz.net>
2024-03-12 16:23:07 +1000

    run build after merging improvement/QHWT-855

--- dist/js/helpers.js
+++ dist/js/helpers.js
@@ -908,7 +908,17 @@ Handlebars.registerHelper('split', function(string, separator) {
 Handlebars.registerHelper('swapLogo', function(desktopLogo, mobileLogo) {
    
     if (typeof window !== 'undefined') { // Check if window exists
+
         let logo = window.innerWidth <= 991 ? mobileLogo : desktopLogo;
+    
+        // Function to update logo based on screen size
+        const updateLogo = () => {
+          logo = window.innerWidth <= 991 ? mobileLogo : desktopLogo;
+        };
+    
+        // Add event listener for screen resize
+        window.addEventListener('resize', updateLogo);
+    
         return logo;
       } else {
         // Handle server-side rendering or non-browser environments
