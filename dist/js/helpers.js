8f43f834b46c4bbb5fcf9ef793a5276cabe0c10b
TJ Flassman <tflassman@squiz.net>
2024-03-13 09:23:30 +1000

    run build after merging improvement/QHWT-855

--- dist/js/helpers.js
+++ dist/js/helpers.js
@@ -905,27 +905,6 @@ Handlebars.registerHelper('split', function(string, separator) {
     }
     else return '';
 }); 
-Handlebars.registerHelper('swapLogo', function(desktopLogo, mobileLogo) {
-   
-    if (typeof window !== 'undefined') { // Check if window exists
-
-        let logo = window.innerWidth <= 991 ? mobileLogo : desktopLogo;
-    
-        // Function to update logo based on screen size
-        const updateLogo = () => {
-          logo = window.innerWidth <= 991 ? mobileLogo : desktopLogo;
-        };
-    
-        // Add event listener for screen resize
-        window.addEventListener('resize', updateLogo);
-    
-        return logo;
-      } else {
-        // Handle server-side rendering or non-browser environments
-        return desktopLogo; // Or some default behavior
-      }
-
-}); 
 Handlebars.registerHelper('toUpperCase', function(string) {
   if(string) {
     return string.toUpperCase();
