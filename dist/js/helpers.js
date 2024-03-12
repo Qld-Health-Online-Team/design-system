257e19107780112d8883780572a59c8cd9b63c7a
TJ Flassman <tflassman@squiz.net>
2024-03-12 15:25:04 +1000

    run build after merging improvement/QHWT-855

--- dist/js/helpers.js
+++ dist/js/helpers.js
@@ -905,6 +905,17 @@ Handlebars.registerHelper('split', function(string, separator) {
     }
     else return '';
 }); 
+Handlebars.registerHelper('swapLogo', function(desktopLogo, mobileLogo) {
+  
+  let logo = desktopLogo; // Default to desktop for wider screens
+
+  // Employ a ternary operator for concise conditional return
+  const isMobileScreen = window.innerWidth <= 991;
+  logo = isMobileScreen ? mobileLogo : logo;
+
+  return logo;
+
+}); 
 Handlebars.registerHelper('toUpperCase', function(string) {
   if(string) {
     return string.toUpperCase();
