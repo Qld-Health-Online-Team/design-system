61c6a0d9f5b5f0e451d6ddc5e359baf6235ef2d5
TJ Flassman <tflassman@squiz.net>
2024-03-13 10:02:29 +1000

    run build after merging improvement/QHWT-855

--- dist/js/helpers.js
+++ dist/js/helpers.js
@@ -905,6 +905,34 @@ Handlebars.registerHelper('split', function(string, separator) {
     }
     else return '';
 }); 
+Handlebars.registerHelper('swapLogo', function (desktopLogo, mobileLogo) {
+    
+    var desktopSize = desktopLogo;
+    var mobileSize = mobileLogo;
+   
+   // function to update image
+    function updateImageSource(){
+        if(window.innerWidth <= 991){
+            return mobileSize;
+        } else {
+            return desktopSize;
+        }
+    }
+
+   // Run the function on window resize
+    window.addEventListener('resize', updateImageSource);
+
+    // Run the function on page load
+    document.addEventListener('DOMContentLoaded', updateImageSource);
+
+
+//     let logo = desktopLogo; 
+
+//     const isMobileScreen = window.innerWidth <= 991;
+//     logo = isMobileScreen ? mobileLogo : logo;
+
+//   return logo;
+}); 
 Handlebars.registerHelper('toUpperCase', function(string) {
   if(string) {
     return string.toUpperCase();
