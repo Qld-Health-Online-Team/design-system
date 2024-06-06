b089c8202175cbc409b13da33d381b919a806e33
QH\wa-najaria <amirhossein.najari@health.qld.gov.au>
2024-06-06 12:46:05 +1000

    promo panel coded - qhwt-1002

--- src/components/promo_panel/js/global.js
+++ src/components/promo_panel/js/global.js
@@ -1,6 +1,45 @@
 (function () {
     'use strict';
 
+    /**
+     * @module initPromoPanel
+     */
+    var initPromoPanel = {};
 
+    /**
+     * Initialise Promo Panel
+     * 
+     * @memberof module:initPromoPanel
+     */
+     initPromoPanel.init = function() {
+
+        if($('.qld__promo_panel').length > 0) {
+            $('section').each(function() {
+                if($(this).hasClass('qld__promo_panel') && !$(this).hasClass('qld__promo_panel--no-image')) {
+                    var next = $(this).next();
+
+                    if($(this).next().is("data")) {
+                        next = $(this).next().next();
+                    }
+
+                    if ($(next).hasClass("qld__body--alt")) {
+                        $(this).addClass("qld__body--alt-half");
+                    }
+
+                    if ($(next).hasClass("qld__body--dark")) {
+                        $(this).addClass("qld__body--dark-half");
+                    }
+
+                    if ($(next).hasClass("qld__body--alt-dark")) {
+                        $(this).addClass("qld__body--alt-dark-half");
+                    }
+                }
+            });
+        }
+    };
+
+    // Assign initPromoPanel to global QLD object of functions
+    QLD.initPromoPanel = initPromoPanel;
     
+    document.addEventListener('DOMContentLoaded', initPromoPanel.init);
 }());
