2b027624e596f4f18a6c9a6f10e1f8fec9142f59
Sanam <sanam.roumianfar@health.qld.gov.au>
2025-01-21 14:31:29 +1000

    Create a video player v2 component

--- src/components/video_player_v2/js/global.js
+++ src/components/video_player_v2/js/global.js
@@ -1,6 +1,16 @@
 (function () {
     'use strict';
 
+    // Select the transcript accordion button
+    const transcriptAccordion = document.querySelector('.qld__video__player-v2__transcript .qld__accordion__title');
 
-    
-}());
+    if (!transcriptAccordion) return;
+
+    transcriptAccordion.addEventListener('click', function () {
+        // Toggle the text content based on the accordion state
+        const buttonText = this.querySelector('i').nextSibling;
+        buttonText.textContent = transcriptAccordion.classList.contains('qld__accordion--open') 
+            ? "Show transcript" 
+            : "Hide transcript";
+    });
+})();
