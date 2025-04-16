3264756626c3e5b5db7bea9624521e64bd956005
Kienvi <kienvi.lam@health.qld.gov.au>
2025-04-16 10:08:24 +1000

    Build dev from fix/QHWT-1279.

--- dist/components/accordion/presentation.js
+++ dist/components/accordion/presentation.js
@@ -6,17 +6,17 @@
         return undefined
     };
 
-  return ((stack1 = lookupProperty(helpers,"if").call(alias1,((stack1 = ((stack1 = (depth0 != null ? lookupProperty(depth0,"metadata") : depth0)) != null ? lookupProperty(stack1,"heading") : stack1)) != null ? lookupProperty(stack1,"value") : stack1),{"name":"if","hash":{},"fn":container.program(2, data, 0, blockParams, depths),"inverse":container.noop,"data":data,"loc":{"start":{"line":5,"column":4},"end":{"line":7,"column":11}}})) != null ? stack1 : "")
+  return ((stack1 = lookupProperty(helpers,"if").call(alias1,((stack1 = ((stack1 = (depth0 != null ? lookupProperty(depth0,"metadata") : depth0)) != null ? lookupProperty(stack1,"heading") : stack1)) != null ? lookupProperty(stack1,"value") : stack1),{"name":"if","hash":{},"fn":container.program(2, data, 0, blockParams, depths),"inverse":container.noop,"data":data,"loc":{"start":{"line":4,"column":12},"end":{"line":6,"column":19}}})) != null ? stack1 : "")
     + "\r\n"
-    + ((stack1 = lookupProperty(helpers,"if").call(alias1,((stack1 = ((stack1 = (depth0 != null ? lookupProperty(depth0,"metadata") : depth0)) != null ? lookupProperty(stack1,"intro") : stack1)) != null ? lookupProperty(stack1,"value") : stack1),{"name":"if","hash":{},"fn":container.program(4, data, 0, blockParams, depths),"inverse":container.noop,"data":data,"loc":{"start":{"line":9,"column":4},"end":{"line":11,"column":11}}})) != null ? stack1 : "")
-    + "    <div class=\"qld__accordion-group "
+    + ((stack1 = lookupProperty(helpers,"if").call(alias1,((stack1 = ((stack1 = (depth0 != null ? lookupProperty(depth0,"metadata") : depth0)) != null ? lookupProperty(stack1,"intro") : stack1)) != null ? lookupProperty(stack1,"value") : stack1),{"name":"if","hash":{},"fn":container.program(4, data, 0, blockParams, depths),"inverse":container.noop,"data":data,"loc":{"start":{"line":8,"column":12},"end":{"line":10,"column":19}}})) != null ? stack1 : "")
+    + "            <div class=\"qld__accordion-group "
     + container.escapeExpression(container.lambda(((stack1 = ((stack1 = (depth0 != null ? lookupProperty(depth0,"metadata") : depth0)) != null ? lookupProperty(stack1,"theme") : stack1)) != null ? lookupProperty(stack1,"value") : stack1), depth0))
     + "\" id=\""
-    + ((stack1 = lookupProperty(helpers,"if").call(alias1,((stack1 = ((stack1 = (depth0 != null ? lookupProperty(depth0,"metadata") : depth0)) != null ? lookupProperty(stack1,"id_field") : stack1)) != null ? lookupProperty(stack1,"value") : stack1),{"name":"if","hash":{},"fn":container.program(6, data, 0, blockParams, depths),"inverse":container.program(8, data, 0, blockParams, depths),"data":data,"loc":{"start":{"line":12,"column":67},"end":{"line":12,"column":163}}})) != null ? stack1 : "")
+    + ((stack1 = lookupProperty(helpers,"if").call(alias1,((stack1 = ((stack1 = (depth0 != null ? lookupProperty(depth0,"metadata") : depth0)) != null ? lookupProperty(stack1,"id_field") : stack1)) != null ? lookupProperty(stack1,"value") : stack1),{"name":"if","hash":{},"fn":container.program(6, data, 0, blockParams, depths),"inverse":container.program(8, data, 0, blockParams, depths),"data":data,"loc":{"start":{"line":11,"column":75},"end":{"line":11,"column":171}}})) != null ? stack1 : "")
     + "\">\r\n"
-    + ((stack1 = (lookupProperty(helpers,"ifCond")||(depth0 && lookupProperty(depth0,"ifCond"))||alias2).call(alias1,((stack1 = ((stack1 = (depth0 != null ? lookupProperty(depth0,"metadata") : depth0)) != null ? lookupProperty(stack1,"toggle_all") : stack1)) != null ? lookupProperty(stack1,"value") : stack1),"==","yes",{"name":"ifCond","hash":{},"fn":container.program(10, data, 0, blockParams, depths),"inverse":container.noop,"data":data,"loc":{"start":{"line":13,"column":8},"end":{"line":20,"column":19}}})) != null ? stack1 : "")
-    + ((stack1 = (lookupProperty(helpers,"printAccordion")||(depth0 && lookupProperty(depth0,"printAccordion"))||alias2).call(alias1,(depth0 != null ? lookupProperty(depth0,"metadata") : depth0),{"name":"printAccordion","hash":{},"fn":container.program(12, data, 0, blockParams, depths),"inverse":container.noop,"data":data,"loc":{"start":{"line":21,"column":12},"end":{"line":37,"column":31}}})) != null ? stack1 : "")
-    + "        \r\n    </div>\r\n";
+    + ((stack1 = (lookupProperty(helpers,"ifCond")||(depth0 && lookupProperty(depth0,"ifCond"))||alias2).call(alias1,((stack1 = ((stack1 = (depth0 != null ? lookupProperty(depth0,"metadata") : depth0)) != null ? lookupProperty(stack1,"toggle_all") : stack1)) != null ? lookupProperty(stack1,"value") : stack1),"==","yes",{"name":"ifCond","hash":{},"fn":container.program(10, data, 0, blockParams, depths),"inverse":container.noop,"data":data,"loc":{"start":{"line":12,"column":16},"end":{"line":19,"column":27}}})) != null ? stack1 : "")
+    + ((stack1 = (lookupProperty(helpers,"printAccordion")||(depth0 && lookupProperty(depth0,"printAccordion"))||alias2).call(alias1,(depth0 != null ? lookupProperty(depth0,"metadata") : depth0),{"name":"printAccordion","hash":{},"fn":container.program(12, data, 0, blockParams, depths),"inverse":container.noop,"data":data,"loc":{"start":{"line":20,"column":20},"end":{"line":36,"column":39}}})) != null ? stack1 : "")
+    + "            </div>\r\n";
 },"2":function(container,depth0,helpers,partials,data) {
     var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
@@ -25,7 +25,7 @@
         return undefined
     };
 
-  return "    <h2>"
+  return "            <h2>"
     + container.escapeExpression(container.lambda(((stack1 = ((stack1 = (depth0 != null ? lookupProperty(depth0,"metadata") : depth0)) != null ? lookupProperty(stack1,"heading") : stack1)) != null ? lookupProperty(stack1,"value") : stack1), depth0))
     + "</h2>\r\n";
 },"4":function(container,depth0,helpers,partials,data) {
@@ -36,7 +36,7 @@
         return undefined
     };
 
-  return "    "
+  return "            "
     + ((stack1 = container.lambda(((stack1 = ((stack1 = (depth0 != null ? lookupProperty(depth0,"metadata") : depth0)) != null ? lookupProperty(stack1,"intro") : stack1)) != null ? lookupProperty(stack1,"value") : stack1), depth0)) != null ? stack1 : "")
     + "\r\n";
 },"6":function(container,depth0,helpers,partials,data) {
@@ -57,7 +57,7 @@
     };
 
   return "accordion-"
-    + container.escapeExpression(((helper = (helper = lookupProperty(helpers,"assetid") || (depth0 != null ? lookupProperty(depth0,"assetid") : depth0)) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"assetid","hash":{},"data":data,"loc":{"start":{"line":12,"column":145},"end":{"line":12,"column":156}}}) : helper)));
+    + container.escapeExpression(((helper = (helper = lookupProperty(helpers,"assetid") || (depth0 != null ? lookupProperty(depth0,"assetid") : depth0)) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"assetid","hash":{},"data":data,"loc":{"start":{"line":11,"column":153},"end":{"line":11,"column":164}}}) : helper)));
 },"10":function(container,depth0,helpers,partials,data) {
     var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
@@ -66,9 +66,9 @@
         return undefined
     };
 
-  return "        <div class=\"qld__accordion__toggle\">\r\n            <button class=\"qld__accordion__toggle-btn qld__accordion__toggle-btn--closed\" type=\"button\" aria-expanded=\"false\">\r\n                Open all\r\n                <svg class=\"accordion-all-svg\" aria-hidden=\"true\" xmlns=\"http://www.w3.org/2000/svg\"><use href=\""
+  return "                <div class=\"qld__accordion__toggle\">\r\n                    <button class=\"qld__accordion__toggle-btn qld__accordion__toggle-btn--closed\" type=\"button\" aria-expanded=\"false\">\r\n                        <span class=\"qld__accordion__toggle-btn-text\">Open all</span>\r\n                        <svg class=\"qld__icon\" aria-hidden=\"true\" xmlns=\"http://www.w3.org/2000/svg\"><use href=\""
     + container.escapeExpression(container.lambda(((stack1 = ((stack1 = ((stack1 = ((stack1 = (data && lookupProperty(data,"root"))) && lookupProperty(stack1,"site"))) && lookupProperty(stack1,"metadata"))) && lookupProperty(stack1,"coreSiteIcons"))) && lookupProperty(stack1,"value")), depth0))
-    + "#chevron-up\"></use></svg>\r\n            </button>\r\n        </div>\r\n";
+    + "#chevron-up\"></use></svg>\r\n                    </button>\r\n                </div>\r\n";
 },"12":function(container,depth0,helpers,partials,data,blockParams,depths) {
     var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
@@ -77,7 +77,7 @@
         return undefined
     };
 
-  return ((stack1 = (lookupProperty(helpers,"ifCond")||(depth0 && lookupProperty(depth0,"ifCond"))||container.hooks.helperMissing).call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? lookupProperty(depth0,"content") : depth0),"!=","",{"name":"ifCond","hash":{},"fn":container.program(13, data, 0, blockParams, depths),"inverse":container.noop,"data":data,"loc":{"start":{"line":22,"column":16},"end":{"line":36,"column":27}}})) != null ? stack1 : "");
+  return ((stack1 = (lookupProperty(helpers,"ifCond")||(depth0 && lookupProperty(depth0,"ifCond"))||container.hooks.helperMissing).call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? lookupProperty(depth0,"content") : depth0),"!=","",{"name":"ifCond","hash":{},"fn":container.program(13, data, 0, blockParams, depths),"inverse":container.noop,"data":data,"loc":{"start":{"line":21,"column":24},"end":{"line":35,"column":35}}})) != null ? stack1 : "");
 },"13":function(container,depth0,helpers,partials,data,blockParams,depths) {
     var stack1, alias1=container.lambda, alias2=container.escapeExpression, lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
@@ -86,25 +86,25 @@
         return undefined
     };
 
-  return "                    <div class=\"qld__accordion\">\r\n                        <"
+  return "                            <div class=\"qld__accordion\">\r\n                                <"
     + alias2(alias1(((stack1 = ((stack1 = (depths[1] != null ? lookupProperty(depths[1],"metadata") : depths[1])) != null ? lookupProperty(stack1,"title_level") : stack1)) != null ? lookupProperty(stack1,"value") : stack1), depth0))
-    + ">\r\n                            <button class=\"qld__accordion__title js-qld__accordion qld__accordion--closed\" aria-controls=\"accordion-group-"
+    + ">\r\n                                    <button class=\"qld__accordion__title js-qld__accordion qld__accordion--closed\" aria-controls=\"accordion-group-"
     + alias2(alias1((depths[1] != null ? lookupProperty(depths[1],"assetid") : depths[1]), depth0))
     + "-"
     + alias2(alias1((depth0 != null ? lookupProperty(depth0,"fieldid") : depth0), depth0))
-    + "\" aria-expanded=\"false\" type=\"button\">\r\n                                "
+    + "\" aria-expanded=\"false\" type=\"button\">\r\n                                        "
     + alias2(alias1((depth0 != null ? lookupProperty(depth0,"title") : depth0), depth0))
-    + "\r\n                                <svg class=\"accordion-svg\" aria-hidden=\"true\" xmlns=\"http://www.w3.org/2000/svg\"><use href=\""
+    + "\r\n                                        <svg class=\"qld__icon qld__icon--md\" aria-hidden=\"true\" xmlns=\"http://www.w3.org/2000/svg\"><use href=\""
     + alias2(alias1(((stack1 = ((stack1 = ((stack1 = ((stack1 = (data && lookupProperty(data,"root"))) && lookupProperty(stack1,"site"))) && lookupProperty(stack1,"metadata"))) && lookupProperty(stack1,"coreSiteIcons"))) && lookupProperty(stack1,"value")), depth0))
-    + "#chevron-up\"></use></svg>\r\n                            </button>\r\n                            </"
+    + "#chevron-up\"></use></svg>\r\n                                    </button>\r\n                                </"
     + alias2(alias1(((stack1 = ((stack1 = (depths[1] != null ? lookupProperty(depths[1],"metadata") : depths[1])) != null ? lookupProperty(stack1,"title_level") : stack1)) != null ? lookupProperty(stack1,"value") : stack1), depth0))
-    + ">\r\n                        <div class=\"qld__accordion__body qld__accordion--closed\" id=\"accordion-group-"
+    + ">\r\n                                <div class=\"qld__accordion__body qld__accordion--closed\" id=\"accordion-group-"
     + alias2(alias1((depths[1] != null ? lookupProperty(depths[1],"assetid") : depths[1]), depth0))
     + "-"
     + alias2(alias1((depth0 != null ? lookupProperty(depth0,"fieldid") : depth0), depth0))
-    + "\">\r\n                            <div class=\"qld__accordion__body-wrapper\">\r\n                                "
+    + "\">\r\n                                    <div class=\"qld__accordion__body-wrapper\">\r\n                                        "
     + ((stack1 = alias1((depth0 != null ? lookupProperty(depth0,"content") : depth0), depth0)) != null ? stack1 : "")
-    + "\r\n                            </div>\r\n                        </div>\r\n                    </div>\r\n";
+    + "\r\n                                    </div>\r\n                                </div>\r\n                            </div>\r\n";
 },"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data,blockParams,depths) {
     var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
@@ -113,7 +113,7 @@
         return undefined
     };
 
-  return "<section class=\"qld__body\">\r\n    <div class=\"container-fluid\">\r\n\r\n"
-    + ((stack1 = lookupProperty(helpers,"with").call(depth0 != null ? depth0 : (container.nullContext || {}),((stack1 = (depth0 != null ? lookupProperty(depth0,"component") : depth0)) != null ? lookupProperty(stack1,"data") : stack1),{"name":"with","hash":{},"fn":container.program(1, data, 0, blockParams, depths),"inverse":container.noop,"data":data,"loc":{"start":{"line":4,"column":0},"end":{"line":40,"column":9}}})) != null ? stack1 : "")
-    + "\r\n</div>\r\n</section>\r\n\r\n";
+  return "<section class=\"qld__body\">\r\n    <div class=\"container-fluid\">\r\n"
+    + ((stack1 = lookupProperty(helpers,"with").call(depth0 != null ? depth0 : (container.nullContext || {}),((stack1 = (depth0 != null ? lookupProperty(depth0,"component") : depth0)) != null ? lookupProperty(stack1,"data") : stack1),{"name":"with","hash":{},"fn":container.program(1, data, 0, blockParams, depths),"inverse":container.noop,"data":data,"loc":{"start":{"line":3,"column":8},"end":{"line":38,"column":17}}})) != null ? stack1 : "")
+    + "    </div>\r\n</section>\r\n\r\n";
 },"useData":true,"useDepths":true}
