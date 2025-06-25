fd51c9d5b84d31fff9938bc48b6fca5c8df1ac6d
Kienvi <kienvi.lam@health.qld.gov.au>
2025-06-25 14:07:13 +1000

    Remove redundant bracket for direction link sb.

--- src/stories/DirectionLinks/DirectionLinks.js
+++ src/stories/DirectionLinks/DirectionLinks.js
@@ -1,3 +1,3 @@
 export const DirectionLinks = ({ text, direction, href }) => {
-    return `<a href="${href}" class="qld__direction-link--${direction}"}>${text}</a>`;
+    return `<a href="${href}" class="qld__direction-link--${direction}">${text}</a>`;
 };
