6f75e7cc00cc4677d68d8f89fff4caaa9ea51a95
Sanam <sanam.roumianfar@health.qld.gov.au>
2025-07-28 10:15:28 +1000

    Update to clean the output dir

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,7 +1,6 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const PrecompilePlugin = require("./PrecompilePlugin");
 // const JsonMergePlugin = require("./JsonMergePlugin");
@@ -46,7 +45,6 @@ module.exports = (env) => {
                 filename: "[name].css",
                 chunkFilename: "[name].css",
             }),
-            new CleanWebpackPlugin(),
             new PrecompilePlugin({
                 input: "./src/components/**/**/*.hbs",
                 manifest: "./src/components/**/js/manifest.json",
