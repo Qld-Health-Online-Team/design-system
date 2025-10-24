d2872586b122f8e48e84c07c5dd782f51097cab0
Kienvi <kienvi.lam@health.qld.gov.au>
2025-10-24 10:49:40 +1000

    LHN js fixes.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-// CSS
-import "./styles/global.scss";
+import initLeftHandNav from "./components/left_hand_navigation/js/global.js";
 
 // JS
 require("prismjs");
+
+// Adding ES module initialisation for components
+document.addEventListener("DOMContentLoaded", () => {
+    initLeftHandNav(document);
+});
+
+// CSS
+import "./styles/global.scss";
