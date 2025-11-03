fbfc92bd4f67d9db968ace2fc87bb637d35f8c61
Kienvi <kienvi.lam@health.qld.gov.au>
2025-11-03 14:23:06 +1000

    Update Header component JS.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-// CSS
-import "./styles/global.scss";
+import initHeader from "./components/header/js/global.js";
 
 // JS
 require("prismjs");
+
+// Adding ES module initialisation for components
+document.addEventListener("DOMContentLoaded", () => {
+    initHeader(document);
+});
+
+// CSS
+import "./styles/global.scss";
