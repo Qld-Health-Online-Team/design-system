1723cd6fd553775e5c007d64e6fe6eda59657988
Kienvi <kienvi.lam@health.qld.gov.au>
2025-12-03 09:15:22 +1000

    Update promo panel icon.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import initHeader from "./components/header/js/global.js";
 import initLeftHandNav from "./components/left_hand_navigation/js/global.js";
+import initPromoPanel from "./components/promo_panel/js/global.js";
 
 // JS
 require("prismjs");
@@ -8,6 +9,7 @@ require("prismjs");
 document.addEventListener("DOMContentLoaded", () => {
     initHeader(document);
     initLeftHandNav(document);
+    initPromoPanel(document);
 });
 
 // CSS
