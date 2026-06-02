f4f01c60680c81864a178917480ff8158352d26c
Kienvi <kienvi.lam@health.qld.gov.au>
2026-06-02 13:24:59 +1000

    Build dev from QHDEV-639.

--- dist/js/helpers.js
+++ dist/js/helpers.js
@@ -854,6 +854,17 @@ Handlebars.registerHelper('printAccordion', function (metadata, options) {
 
     return accordions;
 }); 
+Handlebars.registerHelper('rawEncoded', function (string) {
+    if (!string) return "";
+
+    return string
+        .replace(/<[^>]*>/g, "")
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#x27;");
+}); 
 Handlebars.registerHelper('renderSpecialChar', function(string) {
     var map = {
         '&amp;' : '&',
