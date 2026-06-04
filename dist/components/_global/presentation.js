e00a6216afb6895522ca354c0ba118ed991d1a9c
Alex Hollis <22248105+Parametricall@users.noreply.github.com>
2026-06-05 09:40:25 +1000

    Build develop

--- dist/components/_global/presentation.js
+++ dist/components/_global/presentation.js
@@ -28,23 +28,23 @@
 
   return "<div id=\"ds_component_"
     + alias4(((helper = (helper = lookupProperty(helpers,"id") || (depth0 != null ? lookupProperty(depth0,"id") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data,"loc":{"start":{"line":1,"column":22},"end":{"line":1,"column":28}}}) : helper)))
-    + "\" class=\"loading\">\r\n    <div class=\"loader\">Loading...</div>\r\n\r\n    <script>\r\n\r\n        // Array to store all Datastore components on the page\r\n        var dsComponents = dsComponents ? dsComponents : [];\r\n\r\n        // On Page Load\r\n        window.addEventListener('DOMContentLoaded', function () {\r\n            \r\n            var dsUrl = "
+    + "\" class=\"loading\">\n    <div class=\"loader\">Loading...</div>\n\n    <script>\n\n        // Array to store all Datastore components on the page\n        var dsComponents = dsComponents ? dsComponents : [];\n\n        // On Page Load\n        window.addEventListener('DOMContentLoaded', function () {\n            \n            var dsUrl = "
     + alias4(((helper = (helper = lookupProperty(helpers,"datastore") || (depth0 != null ? lookupProperty(depth0,"datastore") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"datastore","hash":{},"data":data,"loc":{"start":{"line":12,"column":24},"end":{"line":12,"column":37}}}) : helper)))
-    + ".baseURL;\r\n\r\n            dsComponents.push({\r\n                id: '"
+    + ".baseURL;\n\n            dsComponents.push({\n                id: '"
     + alias4(((helper = (helper = lookupProperty(helpers,"id") || (depth0 != null ? lookupProperty(depth0,"id") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data,"loc":{"start":{"line":15,"column":21},"end":{"line":15,"column":27}}}) : helper)))
-    + "',\r\n                dsUrl: dsUrl,\r\n                manifest: "
+    + "',\n                dsUrl: dsUrl,\n                manifest: "
     + ((stack1 = (lookupProperty(helpers,"jsonStringify")||(depth0 && lookupProperty(depth0,"jsonStringify"))||alias2).call(alias1,(depth0 != null ? lookupProperty(depth0,"manifest") : depth0),{"name":"jsonStringify","hash":{},"data":data,"loc":{"start":{"line":17,"column":26},"end":{"line":17,"column":54}}})) != null ? stack1 : "")
-    + ",\r\n                schema: "
+    + ",\n                schema: "
     + ((stack1 = lookupProperty(helpers,"if").call(alias1,(depth0 != null ? lookupProperty(depth0,"schema") : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.program(3, data, 0),"data":data,"loc":{"start":{"line":18,"column":24},"end":{"line":18,"column":83}}})) != null ? stack1 : "")
-    + ",\r\n                schemas: "
+    + ",\n                schemas: "
     + ((stack1 = lookupProperty(helpers,"if").call(alias1,(depth0 != null ? lookupProperty(depth0,"schemas") : depth0),{"name":"if","hash":{},"fn":container.program(5, data, 0),"inverse":container.program(3, data, 0),"data":data,"loc":{"start":{"line":19,"column":25},"end":{"line":19,"column":86}}})) != null ? stack1 : "")
-    + ",\r\n                collection: '"
+    + ",\n                collection: '"
     + alias4(((helper = (helper = lookupProperty(helpers,"collection") || (depth0 != null ? lookupProperty(depth0,"collection") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"collection","hash":{},"data":data,"loc":{"start":{"line":20,"column":29},"end":{"line":20,"column":43}}}) : helper)))
-    + "',\r\n                component: '"
+    + "',\n                component: '"
     + ((stack1 = ((helper = (helper = lookupProperty(helpers,"component") || (depth0 != null ? lookupProperty(depth0,"component") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"component","hash":{},"data":data,"loc":{"start":{"line":21,"column":28},"end":{"line":21,"column":43}}}) : helper))) != null ? stack1 : "")
-    + "',\r\n                renderCallback: '"
+    + "',\n                renderCallback: '"
     + ((stack1 = ((helper = (helper = lookupProperty(helpers,"callback") || (depth0 != null ? lookupProperty(depth0,"callback") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"callback","hash":{},"data":data,"loc":{"start":{"line":22,"column":33},"end":{"line":22,"column":47}}}) : helper))) != null ? stack1 : "")
-    + "',\r\n                type: '"
+    + "',\n                type: '"
     + alias4(((helper = (helper = lookupProperty(helpers,"type") || (depth0 != null ? lookupProperty(depth0,"type") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"type","hash":{},"data":data,"loc":{"start":{"line":23,"column":23},"end":{"line":23,"column":31}}}) : helper)))
-    + "'\r\n            });\r\n        });\r\n        \r\n    </script>\r\n</div>\r\n\r\n\r\n";
+    + "'\n            });\n        });\n        \n    </script>\n</div>\n\n\n";
 },"useData":true}
--- dist/components/abstract/presentation.js
+++ dist/components/abstract/presentation.js
@@ -25,9 +25,9 @@
         return undefined
     };
 
-  return "\r\n<section class=\"qld__body qld__abstract--wrapper\" id=\""
+  return "\n<section class=\"qld__body qld__abstract--wrapper\" id=\""
     + ((stack1 = lookupProperty(helpers,"if").call(depth0 != null ? depth0 : (container.nullContext || {}),((stack1 = ((stack1 = ((stack1 = ((stack1 = (depth0 != null ? lookupProperty(depth0,"component") : depth0)) != null ? lookupProperty(stack1,"data") : stack1)) != null ? lookupProperty(stack1,"metadata") : stack1)) != null ? lookupProperty(stack1,"id_field") : stack1)) != null ? lookupProperty(stack1,"value") : stack1),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.program(3, data, 0),"data":data,"loc":{"start":{"line":2,"column":54},"end":{"line":2,"column":196}}})) != null ? stack1 : "")
-    + "\">\r\n    <div class=\"container-fluid\">\r\n        <div class=\"qld__abstract\">\r\n            "
+    + "\">\n    <div class=\"container-fluid\">\n        <div class=\"qld__abstract\">\n            "
     + ((stack1 = container.lambda(((stack1 = ((stack1 = ((stack1 = ((stack1 = (depth0 != null ? lookupProperty(depth0,"component") : depth0)) != null ? lookupProperty(stack1,"data") : stack1)) != null ? lookupProperty(stack1,"metadata") : stack1)) != null ? lookupProperty(stack1,"content") : stack1)) != null ? lookupProperty(stack1,"value") : stack1), depth0)) != null ? stack1 : "")
-    + "\r\n        </div>\r\n    </div>\r\n</section>\r\n\r\n";
+    + "\n        </div>\n    </div>\n</section>\n\n";
 },"useData":true}
