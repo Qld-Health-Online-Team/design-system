7a95d9b085451d098971961191b9196310c7a020
Kienvi <kienvi.lam@health.qld.gov.au>
2026-06-10 09:27:08 +1000

    QHDEV-667 update renderEncoded.

--- src/helpers/Handlebars/renderEncoded.js
+++ src/helpers/Handlebars/renderEncoded.js
@@ -1,9 +1,14 @@
 module.exports = function (string) {
     if (!string) return "";
+    let cleaned = string;
+    let prev;
 
-    return string
-        .replace(/<[^>]*>/g, "")
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;");
+    while (cleaned !== prev) {
+        prev = cleaned;
+        // Remove any HTML tags
+        cleaned = cleaned.replace(/<[^>]*>/g, "");
+    }
+
+    // Convert the last remaining special characters to HTML entities
+    return cleaned.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
 };
