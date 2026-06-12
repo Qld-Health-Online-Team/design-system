fe8a3b91b3965aa3506e203f76cc53a2302e2952
Alex Hollis <22248105+Parametricall@users.noreply.github.com>
2026-06-12 11:07:36 +1000

    QHDEV-104 - Create stories for internal navigation/ Side navigation

--- src/components/internal_navigation/js/global.js
+++ src/components/internal_navigation/js/global.js
@@ -7,14 +7,12 @@ import {
  * @module internalNavigation
  */
 
-export default function initInternalNavigation(document = document) {
-  const svgSelector = document.querySelector(".qld__side-nav.qld__accordion");
+export default function initInternalNavigation(root = document) {
+  const svgSelector = root.querySelector(".qld__side-nav.qld__accordion");
 
   if (svgSelector) {
     const svgPath = svgSelector.getAttribute("data-path");
-    const thirdLevelLinks = document.querySelectorAll(
-      "ul ul ul a, ul ul ul span",
-    );
+    const thirdLevelLinks = root.querySelectorAll("ul ul ul a, ul ul ul span");
 
     // Loop through each matching element and apply the style
     thirdLevelLinks.forEach((link) => {
