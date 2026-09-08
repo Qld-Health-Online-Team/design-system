418978398671972555e42bcbff904dc75c3db635
kienvi lam <kienvi.lam@health.qld.gov.au>
2026-09-08 11:21:12 +1000

    Update iconography introduction figma link.

--- src/stories/Iconography/Iconography.stories.js
+++ src/stories/Iconography/Iconography.stories.js
@@ -3,7 +3,7 @@ import { Iconography } from "./Iconography";
 import { themeWrapper } from "../../../.storybook/helper-functions.js";
 import iconsSvg from "../../assets/img/QLD-icons.svg?raw";
 
-const componentIntroduction = `<p>All icons are implemented as SVGs and provided through a sprite sheet. Icons used to build components are also available in the Figma UI Kit. We use SVG icons instead of an icon font to improve performance, scalability, maintainability and accessibility.</p>
+const componentIntroduction = `<p>All icons are implemented as SVGs and provided through a sprite sheet. Icons used to build components are also available in the <a href="https://www.figma.com/design/qKsxl3ogIlBp7dafgxXuCA/QGDS-UI-Kit?node-id=6902-69802&p=f&t=4sdNArLVXAEw3eI4-0" target="_blank">Figma UI Kit</a>. We use SVG icons instead of an icon font to improve performance, scalability, maintainability and accessibility.</p>
 <p>You can view all icons included in our icon library below.</p>
 <p>If you require additional icons, you can find them at <a href="https://fonts.google.com/icons" target="_blank">Material Symbols</a>. The following URL defines the specifications for our Material Symbols: https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@40,300,0..1,0.
 You can add Material Symbols manually by including the following &lt;link&gt; tag in the &lt;head&gt; of your page:</p>
